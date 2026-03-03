56babfa433054850dd1e19afcfbee7cf3cf1004e
Tuğrul Düz <tugrul@thelumastudios.com>
2026-03-03 23:29:57 +0300

    enesaga

--- src/pages/Properties.jsx
+++ src/pages/Properties.jsx
@@ -157,7 +157,7 @@ export default function Properties() {
                           d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
                         />
                       </svg>
-                      <span>{property.sqft} {t('common.sqft', 'sqft')}</span>
+                      <span>{property.sqft}</span>
                     </div>
                   </div>
 
